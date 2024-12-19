06bcc64037e8aab3ceaad44789b439f57cff8f4b
Mikkel Olsen <mikkel.olsen@semaphor.dk>
2024-12-19 10:01:27 +0100

    day2 part 1

--- day2/main.go
+++ day2/main.go
@@ -1,9 +1,59 @@
 package day2
 
 import (
+	"adventofcode/utils"
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func Run() {
-	fmt.Print("dag 2")
+	fmt.Println("dag 2")
+
+	content, err := os.Open("day2/input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer content.Close()
+
+	var count int = 0
+	scanner := bufio.NewScanner(content)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		strlist := strings.Fields(line)
+		intlist, err := utils.StringsToIntegers(strlist)
+		if err != nil {
+			fmt.Print(err)
+		}
+		if intlist[1] > intlist[0] && isIncreasing(intlist) {
+			count++
+		} else if intlist[1] < intlist[0] && isDecreasing(intlist) {
+			count++
+		}
+	}
+	fmt.Printf("total number of safe rows: %d \n", count)
+}
+
+func isIncreasing(list []int) bool {
+	for i := 1; i < len(list); i++ {
+		if 0 >= list[i]-list[i-1] || list[i]-list[i-1] > 3 {
+			return false
+		}
+	}
+	return true
+}
+
+func isDecreasing(list []int) bool {
+	for i := 1; i < len(list); i++ {
+		if 0 <= list[i]-list[i-1] || list[i]-list[i-1] < -3 {
+			return false
+		}
+	}
+	return true
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func StringsToIntegers(list []string) ([]int, error) {
+	ints := make([]int, len(list))
+	for i := range list {
+		num, err := strconv.Atoi(list[i])
+		if err != nil {
+			return nil, fmt.Errorf("failed to convert %q to int: %w", list[i], err)
+		}
+		ints[i] = num
+	}
+	return ints, nil
+}
