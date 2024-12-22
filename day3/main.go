533d507570d346cfdeb4e5466b3cef7aafc52a7a
Mikkel Olsen <molybdenum93@gmail.com>
2024-12-22 01:30:11 +0100

    Finished day 3 part 2. remember scope for next time you stupid fuck

--- day3/main.go
+++ day3/main.go
@@ -14,6 +14,8 @@ func Run() {
 	fmt.Println("Day 3")
 	fmt.Println("part 1")
 	part1()
+	fmt.Println("part 2")
+	part2()
 
 }
 
@@ -37,12 +39,56 @@ func part1() {
 		g1 := re.SubexpIndex("g1")
 		g2 := re.SubexpIndex("g2")
 		for i := range matches {
-			fmt.Printf("g1: %q g2: %q\n", matches[i][g1], matches[i][g2])
 			n1, _ := strconv.Atoi(string(matches[i][g1]))
 			n2, _ := strconv.Atoi(string(matches[i][g2]))
-			fmt.Printf("n1: %d n2: %d\n", n1, n2)
 			sum += n1 * n2
 		}
 	}
-	fmt.Printf("Final sum: %d", sum)
+	fmt.Printf("Final sum: %d\n", sum)
+}
+
+func part2() {
+	re := regexp.MustCompile(`(?:mul\((?P<g1>\d{1,3}),(?P<g2>\d{1,3})\)|(?P<action>do\(\)|don't\(\)))`)
+
+	content, err := os.Open("day3/input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer content.Close()
+
+	scanner := bufio.NewScanner(content)
+	var mulEnabled bool = true
+	sum := 0
+	actioncounter := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		matches := re.FindAllSubmatch([]byte(text), -1)
+		g1 := re.SubexpIndex("g1")
+		g2 := re.SubexpIndex("g2")
+		action := re.SubexpIndex("action")
+		for _, match := range matches {
+			//fmt.Printf("g1: %q g2: %q\n", matches[i][g1], matches[i][g2])
+			//fmt.Printf("action: %q\n", matches[i][action])
+			if mulEnabled {
+				n1, _ := strconv.Atoi(string(match[g1]))
+				n2, _ := strconv.Atoi(string(match[g2]))
+				sum += n1 * n2
+			}
+			if string(match[action]) == "do()" {
+				actioncounter++
+				//fmt.Println("---------------MUL ENABLED------------------")
+				mulEnabled = true
+			}
+			if string(match[action]) == "don't()" {
+				actioncounter++
+				//fmt.Println("---------------MUL DISABLED-----------------")
+				mulEnabled = false
+			}
+			//fmt.Printf("sum is: %d\n Number of do's and don'ts: %d\n", sum, actioncounter)
+		}
+	}
+	fmt.Printf("Final sum: %d\n", sum)
 }
